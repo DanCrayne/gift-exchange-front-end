306e3e415166cda6a07ca01e8a1c27f10e34334d
crayne <crayne@t570dc.localdomain>
2017-08-26 12:51:30 -0700

    Add ability to randomize and send messages and select time/date

--- debug/debug.js
+++ debug/debug.js
@@ -14,5 +14,18 @@ jQuery('#sendMessages').click(function() {
   jQuery.post(hostUrl + '/events/1/sendmsgs') 
     .done(function(result) {
       jQuery('#output').html(JSON.stringify(result));
+      for (pair of result) {
+        console.log(JSON.stringify(pair));
+      }
+    })
+});
+
+jQuery('#sendMessage').click(function() {
+  jQuery.post(hostUrl + '/events/1/sendmsgs/1') 
+    .done(function(result) {
+      jQuery('#output').html(JSON.stringify(result));
+      for (pair of result) {
+        console.log(JSON.stringify(pair));
+      }
     })
 });
--- resources/js/index.js
+++ resources/js/index.js
@@ -9,8 +9,18 @@ jQuery(document).ready(function() {
   clearAllFormValues();
   disableAllFormInputs();
   enableEventSelectInput();
+
+  jQuery(function() {
+    jQuery('#eventForm input[name=date]').datepicker();
+  });
+
+  jQuery(function() {
+    jQuery('#eventForm input[name=time]').timepicker();
+  });
 });
 
+
+// *** Event Modification ***
 
 jQuery("#eventSelect").change(function() {
   eventId = jQuery("#eventSelect").val();
@@ -102,6 +112,94 @@ jQuery('#createEvent').click(function() {
 });
 
 
+// *** Update Event ***
+jQuery('#updateEvent').click(function() {
+  eventFormData = jQuery('#eventForm').serialize();
+  eventFormData += '&dateOccurs=' 
+                 +       jQuery('#eventForm input[name=date]').val()
+                 + ' ' + jQuery('#eventForm input[name=time]').val();
+  eventFormData += '&adminId=' + loginId;
+
+  jQuery.ajax({
+    url: hostUrl + '/events/' + eventId
+  , method: 'PUT'
+  , crossDomain: true
+  , dataType: 'text'
+  , data: eventFormData
+  , success: function(response) {
+      jQuery('#eventResult').addClass('alert alert-success');
+      jQuery('#eventResult').html('<strong>Event Updated</strong>');
+      jQuery('#eventResult').fadeTo(2000, 500).slideUp(500, function() {
+        jQuery('#eventResult').slideUp(500);
+      });
+    }
+  , error: function(response) {
+      jQuery('#eventResult').addClass('alert alert-danger');
+      jQuery('#eventResult').html('<strong>Error: </strong>cannot update event');
+      jQuery('#eventResult').fadeTo(2000, 500).slideUp(500, function() {
+        jQuery('#eventResult').slideUp(500);
+      });
+  }
+  });
+});
+
+
+// *** Delete Event ***
+
+jQuery('#deleteEvent').click(function() {
+
+  jQuery.ajax({
+    url: hostUrl + '/events/' + eventId
+  , method: 'DELETE'
+  , crossDomain: true
+  , dataType: 'text'
+  , success: function(response) {
+      jQuery('#eventResult').addClass('alert alert-success');
+      jQuery('#eventResult').html('<strong>Event Deleted</strong>');
+      jQuery('#eventResult').fadeTo(2000, 500).slideUp(500, function() {
+        jQuery('#eventResult').slideUp(500);
+      });
+      refreshEventSelectionList();
+      clearAllFormValues();
+      disableAllFormInputs();
+      enableEventSelectInput();
+    }
+
+  , error: function(response) {
+      jQuery('#eventResult').addClass('alert alert-danger');
+      jQuery('#eventResult').html('<strong>Error: </strong>Cannot Delete Event');
+      jQuery('#eventResult').fadeTo(2000, 500).slideUp(500, function() {
+        jQuery('#eventResult').slideUp(500);
+      });
+    }
+  });
+
+});
+
+
+// *** randomize participant (giver, receiver) pairs and send messages ***
+
+jQuery('#randomize').click(function() {
+  if (window.confirm("This operation cannot be reversed! All participants will be sent messages containing their recipients. Any changes from this point will require re-assignment of gift recipients. Are you sure you want to continue?")) {
+
+    jQuery.post(hostUrl + '/events/' + eventId + '/randomize') 
+      .then(function(result) {
+        return jQuery.post(hostUrl + '/events/' + eventId + '/sendmsgs')
+      })
+
+      .then(function(result) {
+        for (pair of result) {
+          console.log(JSON.stringify(pair));
+        }
+      })
+
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
+});
+
+
 // *** Helper Functions ***
 
 function removeQuotes(str) {
@@ -119,15 +217,34 @@ function refreshEventFormInputs() {
   jQuery.get(hostUrl + '/events/' + eventId, function(result) {
   })
     .done(function(result) {
-      jQuery('#eventForm input[name=name]')           .val(removeQuotes(result.name));
-      jQuery('#eventForm textarea[name=description]') .val(removeQuotes(result.description));
-      jQuery('#eventForm input[name=maxGiftPrice]')   .val(result.max_gift_price);
-      jQuery('#eventForm input[name=street]')         .val(removeQuotes(result.loc_street));
-      jQuery('#eventForm input[name=city]')           .val(removeQuotes(result.loc_city));
-      jQuery('#eventForm input[name=state]')          .val(removeQuotes(result.loc_state));
-      jQuery('#eventForm input[name=zipcode]')        .val(removeQuotes(result.loc_zipcode));
-      jQuery('#eventForm input[name=date]')           .val(removeQuotes(result.occurs));
+      jQuery('#eventForm input[name=name]')           
+        .val(removeQuotes(result.name));
+
+      jQuery('#eventForm textarea[name=description]') 
+        .val(removeQuotes(result.description));
+
+      jQuery('#eventForm input[name=maxGiftPrice]')   
+        .val(result.max_gift_price);
+
+      jQuery('#eventForm input[name=street]')
+        .val(removeQuotes(result.loc_street));
+
+      jQuery('#eventForm input[name=city]')
+        .val(removeQuotes(result.loc_city));
+
+      jQuery('#eventForm input[name=state]')
+        .val(removeQuotes(result.loc_state));
+
+      jQuery('#eventForm input[name=zipcode]')
+        .val(removeQuotes(result.loc_zipcode));
+
+      jQuery('#eventForm input[name=date]')
+        .val(removeQuotes(result.occurs).split(' ')[0]);
+
+      jQuery('#eventForm input[name=time]')
+        .val(removeQuotes(result.occurs).split(' ')[1]);
     })
+
     .fail(function() {
       alert('Could not get event info');
     });
@@ -164,12 +281,17 @@ function refreshEventUsersTable() {
 
   jQuery.get(hostUrl + '/events/' + eventId + '/users', function(result) {
 
+    // TODO: implement modify and delete actions
     for (user of result) {
       userRows += '<tr>' 
                 + '<td>' + JSON.stringify(user["id"]) + '</td>'
                 + '<td>' + removeQuotes(JSON.stringify(user["first_name"])) + '</td>'
                 + '<td>' + removeQuotes(JSON.stringify(user["last_name"]))  + '</td>'
                 + '<td>' + removeQuotes(JSON.stringify(user["email_addr"])) + '</td>'
+                + '<td>' 
+                + '<button name="modifyUser" value="' + removeQuotes(JSON.stringify(user["id"])) + '" class="btn btn-default" disabled>Modify</button>'
+                + '<button name="deleteUser" value="' + removeQuotes(JSON.stringify(user["id"])) + '" class="btn btn-default" disabled>Delete</button>'
+                + '</td>'
                 + '</tr>';
     }
   })
@@ -201,6 +323,7 @@ function switchToUpdateEventMode() {
   enableAddUserButton();
   enableAddUserFormInputs();
   enableEventFormTextInputs();
+  enableRandomizeButton();
 }
 
 
@@ -248,6 +371,10 @@ function enableDeleteEventButton() {
 
 function enableAddUserButton() {
   jQuery('#addUser').prop('disabled', false);
+}
+
+function enableRandomizeButton() {
+  jQuery('#randomize').prop('disabled', false);
 }
 
 function enableAllFormInputs() {
